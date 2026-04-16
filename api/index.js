8a9207e39c16e6236960a971cb23f76167c068a5
Rubenvanega22 <133234907+Rubenvanega22@users.noreply.github.com>
2026-04-15 22:59:38 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2282,7 +2282,7 @@ async function apiGetInventarioByDay(p, res) {
     const cortesiasAyer=(salesAyer||[]).filter(s=>s.product_id===prod.id&&s.is_cortesia).reduce((a,s)=>a+Number(s.cantidad||0),0);
     const entradasAyer=(entriesAyer||[]).filter(e=>e.product_id===prod.id).reduce((a,e)=>a+Number(e.cantidad||0),0);
     const saldoInicial=Number(prod.stock_actual||0)+totalVentas+totalCortesias-totalEntradas;
-    const saldoInicialReal=saldoInicial+ventasAyer+cortesiasAyer-entradasAyer;
+    const saldoInicialReal=saldoInicial;
     const turnosData={};
     shifts.forEach(function(sid){
       const ent=(entries||[]).filter(e=>e.product_id===prod.id&&e.shift_id===sid).reduce((a,e)=>a+Number(e.cantidad||0),0);
